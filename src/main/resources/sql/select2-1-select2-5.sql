cec674b09a3d540301066ca1c5f6f36f545d7988
AVM82 <1982alexvm@gmail.com>
2018-01-07 21:54:55 +0200

    #60: created populate.sql with a query to populate the database with test data

--- src/main/resources/sql/select2-1-select2-5.sql
+++ src/main/resources/sql/select2-1-select2-5.sql
@@ -1,12 +1,12 @@
 -- Find the names of buyers, whose age is from 16 to 35
-SELECT b.name
-FROM buyer b
-WHERE b.age BETWEEN 16 AND 35;
+SELECT c.name
+FROM customers c
+WHERE c.age BETWEEN 16 AND 35;
 
--- Find the names of buyers whose last name ends in 'OV'
-SELECT b.name
-FROM buyer b
-WHERE b.surname ILIKE '%ov';
+-- Find the names of buyers whose surname ends in 'OV'
+SELECT c.name
+FROM customers c
+WHERE c.surname ILIKE '%ov';
 
 -- Find the name of the most expensive item for purchase,
 -- in the name of which there is the letter 'V', but not the first and not the last one.
@@ -23,14 +23,13 @@ GROUP BY products.name;
 
 --Find the names of buyers whose names contain the letter 'V' and no more than two times
 --For a regular expression, thanks to @kortov
-SELECT
-  b.name
-FROM buyer b
-WHERE b.name SIMILAR TO '([^Vv]*[Vv][^Vv]*){0,2}';
+SELECT c.name
+FROM customers c
+WHERE c.name SIMILAR TO '([^Vv]*[Vv][^Vv]*){0,2}';
 
 -- Find the names of buyers whose name length is more than 3 characters and 4 character are 'O' (Latin letter O)
 -- and age less than 50 years.
-SELECT b.name
-FROM buyer b
-WHERE (b.name ILIKE '___O%') AND (b.age < 50)
+SELECT c.name
+FROM customers c
+WHERE (c.name ILIKE '___O%') AND (c.age < 50)
 
